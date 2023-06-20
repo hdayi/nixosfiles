15092ccdb271f9b7ef2ac00e23730409491652a9
Your Name <you@example.com>
2023-06-20 12:17:11 +0300

    migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -4,3 +4,7 @@ sudo mv /etc/nixos/configuration.nix /etc/nixos/configuration.nix.old
 sudo mv /etc/nixos/hardware-configuration.nix /etc/nixos/hardware-configuration.nix.old
 sudo ln -s ~/.config/home-manager/configuration.nix /etc/nixos/configuration.nix
 sudo ln -s ~/.config/home-manager/hardware-configuration.nix /etc/nixos/hardware-configuration.nix
+
+#git setup
+git config --global user.email "hdayi@hotmail"
+git config --global user.name "HACI DAYI"
